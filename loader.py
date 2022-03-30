c6ba43f8be6c38fee76e475a8e8001c6018a4447
Samuel C <s4mc4d@outlook.com>
2022-03-31 01:15:08 +0400

    working basic upload file object

--- loader.py
+++ loader.py
@@ -1,9 +1,10 @@
 """This script takes a list of files from a given directory and uploads to AWS S3
 Make sure that the following environment variables are defined :
-        AWS_USERNAME
-        AWS_S3_URI
-        AWS_ACCESS_KEY 
-        AWS_SECRET_KEY
+    AWS_USERNAME
+    AWS_S3_URI
+    AWS_S3_BUCKET_NAME
+    AWS_ACCESS_KEY_ID
+    AWS_SECRET_KEY
 """
 import io
 import os
@@ -11,42 +12,36 @@ import os
 import boto3
 import pandas
 from botocore.exceptions import ClientError
+import logging
 
-
-class AWSBucketManager():
-    def __init__(self) -> None:
-        self.s3 = boto3.resource("s3")
-        # self.bucket_uri = os.getenv("AWS_S3_URI")
-        self.bucket = self.s3.Bucket(self.bucket_name)
-
-        self.session = boto3.Session(aws_access_key_id=os.getenv("AWS_ACCESS_KEY"), 
-                        aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
-                        region_name='eu-west-3')
-
-s3 = session.resource("s3")
-
-# Print out bucket names
-# print("List of buckets available")
-# for bucket in s3.buckets.all():
-#     print(bucket.name)
-
-
-s3_client = boto3.client('s3', region_name='us-east-1', aws_access_key_id=ACCESS_KEY,
-                               aws_secret_access_key=ACCESS_SECRET)
-
-
-def upload_my_file(bucket, folder, file_as_binary, file_name):
-        file_as_binary = io.BytesIO(file_as_binary)
-        key = folder+"/"+file_name
+class AwsS3Facade():
+    def __init__(self,aws_access_key_id,aws_secret_access_key) -> None:
+        self._region = "us-east-1"
+        self.s3_client = boto3.client('s3', 
+                                region_name=self._region,  #'eu-west-3', 
+                                aws_access_key_id=aws_access_key_id,
+                                aws_secret_access_key=aws_secret_access_key)
+    
+    def upload_file(self, file_path, bucket_name, key):
         try:
-            s3_client.upload_fileobj(file_as_binary, bucket, key)
+            self.s3_client.upload_file(file_path, bucket_name, key)
         except ClientError as e:
-            print(e)
+            logging.error(e)
             return False
         return True
 
 
-#get file as binary
-file_binary = open("/home/user/Documents/test.html", "rb").read()
-#uploading file
-upload_my_file("bucket-name", "folder-name", file_binary, "test.html")
+if __name__=="__main__":
+    file_binary = open("/Users/sam/Documents/DEV/JEDHA/PROJET_FINAL/data_collection_yfinance/data/raw/all_tickers_1d.pkl", "rb")
+
+    s3_client = boto3.client('s3', 
+                                region_name="us-east-1",  #'eu-west-3', 
+                                aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID"),
+                                aws_secret_access_key=os.getenv("AWS_SECRET_KEY"))
+    try:
+        s3_client.upload_fileobj(file_binary,os.getenv("AWS_S3_BUCKET_NAME"), "datalake/toto.pkl")
+    except ClientError as e:
+        print(e)
+    finally:
+        file_binary.close()
+    
