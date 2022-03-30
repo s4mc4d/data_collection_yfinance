6d0904d223113bd7f3a93ce47bca485251a10b17
Samuel C <s4mc4d@outlook.com>
2022-03-30 23:33:20 +0400

    updated config and modified loader

--- load_config.py
+++ load_config.py
@@ -7,7 +7,7 @@ with open("config.yml", 'r') as stream:
     # Reading each constant.
     RAW_FOLDER = data_loaded.get(data_loaded["RAW_FOLDER"],"./data/raw")
     INTERMEDIATE_FOLDER = data_loaded.get(data_loaded["INTERMEDIATE_FOLDER"],"./data/intermediate")
-    YF_INTERVAL = data_loaded.get(data_loaded["YF_INTERVAL"],"1m") # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo (optional, default is '1d')
+    YF_INTERVAL = data_loaded["YF_INTERVAL"] # valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo (optional, default is '1d')
     USE_CACHED = data_loaded.get("USE_CACHED",False)
     END_DAY = data_loaded["END_DAY"]
     SYMBOLS_MAPPING = data_loaded["symbols"]
--- loader.py
+++ loader.py
@@ -5,17 +5,19 @@ Make sure that the following environment variables are defined :
         AWS_ACCESS_KEY 
         AWS_SECRET_KEY
 """
-import pandas
-import boto3
-import os
 import io
+import os
+
+import boto3
+import pandas
+from botocore.exceptions import ClientError
 
 
 class AWSBucketManager():
     def __init__(self) -> None:
         self.s3 = boto3.resource("s3")
-        self.bucket_name = os.environ["AWS_S3_URI"]        
-
+        # self.bucket_uri = os.getenv("AWS_S3_URI")
+        self.bucket = self.s3.Bucket(self.bucket_name)
 
         self.session = boto3.Session(aws_access_key_id=os.getenv("AWS_ACCESS_KEY"), 
                         aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
@@ -24,43 +26,27 @@ class AWSBucketManager():
 s3 = session.resource("s3")
 
 # Print out bucket names
-print("List of buckets available")
-for bucket in s3.buckets.all():
-    print(bucket.name)
-
-# Selecting bucket
-bucket = s3.create_bucket(Bucket="bucket-from-notebook-jedha-1917")
-
-import pandas as pd
-data = pd.DataFrame({'col1': [1,2,3,4], 'col2': ['a1','a2','a3','a4']})
-
-csv = data.to_csv()
-put_object = bucket.put_object(Key="test.csv", Body=csv)
+# print("List of buckets available")
+# for bucket in s3.buckets.all():
+#     print(bucket.name)
 
 
+s3_client = boto3.client('s3', region_name='us-east-1', aws_access_key_id=ACCESS_KEY,
+                               aws_secret_access_key=ACCESS_SECRET)
 
-def upload_file_to_S3_bucket(file_path, bucket_name, s3_key):
-    """Uploads a file to an S3 bucket
 
-    Parameters
-    ----------
-    file_path : str
-        Path to file that will be uploaded
-    bucket_name : str
-        Name of the S3 bucket that will hold the file
-    s3_key : str
-        Key to access the file in the bucket
+def upload_my_file(bucket, folder, file_as_binary, file_name):
+        file_as_binary = io.BytesIO(file_as_binary)
+        key = folder+"/"+file_name
+        try:
+            s3_client.upload_fileobj(file_as_binary, bucket, key)
+        except ClientError as e:
+            print(e)
+            return False
+        return True
 
-    Returns
-    -------
-    str
-        URL of the uploaded file
-    """
-    s3_client = session.client("s3")
-    file_name = file_path.split("/")[-1]
-    with open(file_path, "rb") as f:
-        s3_client.upload_fileobj(f, bucket_name, s3_key + file_name)
 
-    # Build the url
-    url = "https://s3.eu-west-3.amazonaws.com/{}/{}".format(bucket_name, s3_key + file_name)
-    return url
+#get file as binary
+file_binary = open("/home/user/Documents/test.html", "rb").read()
+#uploading file
+upload_my_file("bucket-name", "folder-name", file_binary, "test.html")
